d2b65da215d1e0b044074c789011e77ddb3af802
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-10-26 19:30:24 +0100

    updating the controller

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,34 +1,35 @@
 const {StatusCodes} = require('http-status-codes');
 
-const database = require('../db/storage');
+
+const { addAccount,
+    getAccountByNUm,
+    getAllAccounts } = require('../services/accountService');
 const {NotFound, BadRequest} = require('../utils/errors');
 const generateNum = require('../utils/generate-num');
 const validateInput = require('../utils/validation');
 
 const createAccount = async (req, res) => {
-    const { error, value } = validateInput(req.body);
-
+    const {error, value } = validateInput(req.body);
     if (error) {
         throw new BadRequest(error.details[0].message);
     };
 
-    const randomNum = generateNum(10);
-    value.accountNumber = parseInt(randomNum);   // add account number
-    database[database.length] = {...value};
+    const accountNum = await generateNum();
+    console.log(accountNum)
+    // ensures uniqueness by searching the db
+    while (await getAccountByNUm(accountNum) != null) {
+        accountNum = await generateNum();
+    }; 
+    value.accountNumber = accountNum;   // add account number
+    await addAccount(value) // create account number
     delete value.dob;  // don't return the DOB
 
     res.status(StatusCodes.CREATED).json(value);
 };
 
 const getAccountByID = async (req, res) => {
-    let accountInfo;
-    for (let i=0;i<database.length;i++) {
-        if (parseInt(req.params['acctNum']) == database[i].accountNumber) {
-            accountInfo = database[i];
-            break;
-        };
-    };
-    if (!accountInfo) {
+    const accountInfo = await getAccountByNUm(req.params['acctNum'])
+    if (accountInfo == null) {
         throw new NotFound(`Account ${req.params['acctNum']} not found`);
     };
 
@@ -36,7 +37,8 @@ const getAccountByID = async (req, res) => {
 };
 
 const getAccounts = async (req, res) => {
-    res.status(StatusCodes.OK).json(database);
+    const allAccounts = await getAllAccounts();
+    res.status(StatusCodes.OK).json(allAccounts);
 };
 
 module.exports = {
